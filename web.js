b4061d6e96995bcee9d4357de552e9473507eb5d
jaimerr <lleims@yahoo.com>
2014-04-17 14:39:53 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -1,18 +1,24 @@
 var fs = require('fs');
-var express = require('express');
+// var express = require('express');
+var http = require('http');
 var htmlfile = "index.html";
-var app = express.createServer(express.logger());
+// var app = express.createServer(express.logger());
 
 
-app.get('/', function(request, response) {
+var server = http.createServer(function(request, response) {
 var html = fs.readFileSync(htmlfile).toString();
-// var output = new Buffer(1000);
-// output = fs.readFileSync('index.html');
-response.send(html);
+var output = new Buffer(1000);
+output = fs.readFileSync('index.html');
+//response.head(200, {"Content-type": "text/plain"});
 
+response.write(output);
+response.end();
 });
-
+server.listen(8080);
+/*
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+console.log('Listening');
